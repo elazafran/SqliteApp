3e0007c86e00cbb0fcdb765f986a628f974cacc1
Javier Aliaga <javiealiaga@gmail.com>
2018-01-31 17:00:26 +0100

    creamos la bbdd sqllite

--- app/src/main/java/com/example/elaza/sqliteapp/DatabaseHelper.java
+++ app/src/main/java/com/example/elaza/sqliteapp/DatabaseHelper.java
@@ -28,15 +28,20 @@ public class DatabaseHelper extends SQLiteOpenHelper{
      */
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
     }
 }
--- app/src/main/java/com/example/elaza/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/elaza/sqliteapp/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //este contructor nos crea la base de datos con las constantes definidas en la clase
+        myDb = new DatabaseHelper(this);
+
     }
+
 }
