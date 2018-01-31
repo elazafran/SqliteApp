8e5cbb43aa750bdfee9e0f35f8fb4ee89d4dd4f1
Javier Aliaga <javiealiaga@gmail.com>
2018-01-31 16:35:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/elaza/sqliteapp/DatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.elaza.sqliteapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by elaza on 31/01/2018.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
+
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use to open or create the database
+     */
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
